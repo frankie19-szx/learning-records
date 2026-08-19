19a3751b173f37f2492540ea2a0c29344f5321eb
frankie19-szx <2827137955@qq.com>
2026-08-19 11:35:32 +0800

    更新README.md，添加学生成绩管理系统设计描述

--- 8.19/学生成绩管理系统.c
+++ 8.19/学生成绩管理系统.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
 
+typedef struct{
+    int id;
+    char name[20];
+    int scores[5];
+    float avg;
+}student_t;
+
+typedef struct{
+    student_t *data;
+    int length;
+    int capacity;
+}seq_list_t;
+
+typedef struct fail_node{
+    int student_id;
+    struct fail_node *next;
+}fail_node_t;
+
+typedef struct{
+    fail_node_t *head;
+    int count;
+}fail_list_t;
+
+typedef enum{
+    OP_ADD=1,
+    OP_DEL=2,
+    OP_MOD=3
+}op_type_t;
+
+typedef struct{
+    op_type_t type;
+    student_t old_data;
+    int index;
+}undo_item_t;
 
 
 
